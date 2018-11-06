d188efd8e9fdcc630aa629f1ac2344a0b37a817e
mirmik <mirmikns@yandex.ru>
2018-11-06 14:54:35 +0500

    a

--- models/engineroom/top_assemble.py
+++ models/engineroom/top_assemble.py
@@ -10,9 +10,9 @@ from raspbase import raspbox
 
 def top_assemble():
  	scene = Scene()
- 	scene.add(top_model().eval(), Color(0,1,0))
- 	scene.add(ardu_position(ardubox()).eval(), Color(0.6,0.6,0.8))
- 	scene.add(rasp_position(raspbox()).eval(), Color(0.6,0.6,0.8))
+ 	scene.add(top_model().unlazy(), Color(0,1,0))
+ 	scene.add(ardu_position(ardubox()).unlazy(), Color(0.6,0.6,0.8))
+ 	scene.add(rasp_position(raspbox()).unlazy(), Color(0.6,0.6,0.8))
  	return scene
 
 if __name__ == "__main__":
