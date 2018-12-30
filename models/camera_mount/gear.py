6793447d984a07504b8ad8d95f8568deeb7e9b00
mirmik <mirmikns@yandex.ru>
2018-12-30 23:30:12 +0300

    a

--- models/camera_mount/gear.py
+++ models/camera_mount/gear.py
@@ -7,8 +7,8 @@ n = 20
 ri = 3.8 / 2
 ro = 5.2 / 2
 
-angles1 = [gr(360)/n*i for i in range(0,n)]
-angles2 = [gr(360)/n*i+gr(360)/n/2 for i in range(0,n)]
+angles1 = [deg(360)/n*i for i in range(0,n)]
+angles2 = [deg(360)/n*i+deg(360)/n/2 for i in range(0,n)]
 
 r0 = [point3(ri*math.cos(a), ri*math.sin(a)) for a in angles1]
 r1 = [point3(ro*math.cos(a), ro*math.sin(a)) for a in angles2]
--- models/camera_mount/servo2_mount.py
+++ models/camera_mount/servo2_mount.py
@@ -12,7 +12,7 @@ ssize = (5, t, miniservo_width + t)
 shole_radius = miniservo_mount_radius
 shole_up = t + miniservo_width / 2 
 
-s = box(size = ssize) - cylinder(h=t, r=shole_radius).rotateX(gr(90)).up(shole_up).forw(t).right(ssize[0]/2)
+s = box(size = ssize) - cylinder(h=t, r=shole_radius).rotateX(deg(90)).up(shole_up).forw(t).right(ssize[0]/2)
 
 m = (
 	box(miniservo_length + 5 * 2, miniservo_height, t) 
