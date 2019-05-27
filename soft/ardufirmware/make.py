0d115118a8d700f46a62d1a021c28899640d1e12
mirmik <mirmikns@yandex.ru>
2019-05-28 00:54:01 +0300

    'a'

--- soft/ardufirmware/make.py
+++ soft/ardufirmware/make.py
@@ -22,15 +22,15 @@ def install_retrans():
 	time.sleep(1)
 	os.system("bash /home/mirmik/start-trans.sh &")
 
-remote_ip = "192.168.1.145"
+remote_ip = "192.168.1.93"
 
 @licant.routine(deps=['firmware'])
 def remote_install():
-	os.system("ctrans .12.{ip}:10008 --pulse exit".format(ip=remote_ip))
+	os.system("ctrans .12.{ip}:10010 --pulse exit".format(ip=remote_ip))
 	os.system("scp ./firmware mirmik@{ip}:/tmp/enginedrive.bin".format(ip=remote_ip))
 	os.system("ssh mirmik@{ip} avrdude -P/dev/ttyACM0 -v -carduino -patmega328p -b115200 -D -Uflash:w:/tmp/enginedrive.bin -u".format(ip=remote_ip))
 	time.sleep(1)
-	os.system("ssh mirmik@{ip} ctrans --api --noconsole --udp 10008 --serial /dev/ttyACM0 > /dev/null &".format(ip=remote_ip))
+	os.system("ssh mirmik@{ip} ctrans --api --noconsole --udp 10010 --serial /dev/ttyACM0 > /dev/null &".format(ip=remote_ip))
 
 licant.glbfunc.genos_firmware(
 	sources=["main.cpp"],
