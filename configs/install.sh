07b70a53f2938d316d41bc06d31cdc5e47649652
mirmik <mirmikns@yandex.ru>
2022-07-29 00:03:59 +0300

    'a'

--- configs/install.sh
+++ configs/install.sh
@@ -1,4 +1,6 @@
 mkdir -p /etc/rfdaemon
 
+sudo systemctl stop rfdaemon.service
 cp rfdaemon-applicatons.json /etc/rfdaemon/applications.json
 cp reproxy.yml /home/mirmik/.reproxy.yml
+sudo systemctl start rfdaemon.service
