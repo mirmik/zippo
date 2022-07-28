dbeba918cec428a424b8770b182023d4b11ef5d2
mirmik <mirmikns@yandex.ru>
2022-07-28 09:40:49 +0300

    'a'

--- configs/install.sh
+++ configs/install.sh
@@ -1,4 +1,4 @@
 mkdir -p /etc/rfdaemon
 
-cp rfdaemon-applicatons.json /etc/rfdaemon/applicatons.json
+cp rfdaemon-applicatons.json /etc/rfdaemon/applications.json
 cp reproxy.yml /home/mirmik/.reproxy.yml
